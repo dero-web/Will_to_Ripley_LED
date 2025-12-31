aa5e5bdf53d68f6dcdfd03cd8e95898492326fc9
deroboy <129556610+dero-web@users.noreply.github.com>
2025-12-31 11:51:42 +1000

    Update config.php

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php
 // MQTT Config
-$mqtt_server   = 'HIVEMQ_URL_HIER';
+$mqtt_server   = 'c3965b4b38e246fb9a6414bbefa3a2b5.s1.eu.hivemq.cloud';
 $mqtt_port     = 8883;
-$mqtt_username = 'HIVEMQ_CREDENTIALS_USERNAME_HIER';
-$mqtt_password = 'HIVEMQ_CREDENTIALS_PASSWORT_HIER';
+$mqtt_username = 'Steve';
+$mqtt_password = 'NLjFV@H4anvdtpC';
 $mqtt_topic    = 'strangerthings/led';
